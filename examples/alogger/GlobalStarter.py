46476ed46fcd9b2ed9ec75be9cccf79054452a52
kasicass <kasicass@gmail.com>
2018-02-08 21:07:54 +0800

    see log runs

--- examples/alogger/GlobalStarter.py
+++ examples/alogger/GlobalStarter.py
@@ -1,17 +1,19 @@
 # -*- encoding: utf-8 -*-
 from fatkernel import Actor
+from fatkernel import LogManager
 from fatkernel.interface import IStartup
 
 class GlobalStarter(Actor, IStartup):
 	def __init__(self, args):
 		super(GlobalStarter, self).__init__(args)
+		self.logger = LogManager.getLogger(self.__class__.__name__)
 
 	def applicationStartup(self):
-		print '[Starter] applicationStartup'
+		self.logger.info('applicationStartup, %d', 10)
 		self.createRepeatTimer(self.makeSomeLog, 1, 10)
 
 	def applicationShutdown(self):
-		print '[Starter] applicationShutdown'
+		self.logger.info('applicationShutdown, %d', 10)
 
 	def makeSomeLog(self, handle):
 		self.logger.info('hello logger')
--- examples/alogger/main.py
+++ examples/alogger/main.py
@@ -1,7 +1,11 @@
 # -*- encoding: utf-8 -*-
+from fatkernel import LogManager
 from fatkernel import ActorContainer
 
 def main():
+	logger = LogManager.getLogger('main')
+	logger.info('----------------')
+
 	ac = ActorContainer()
 	ac.createActor('GlobalStarter')
 	ac.run()
